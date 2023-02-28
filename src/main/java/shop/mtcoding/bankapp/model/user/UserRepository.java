011e706a6384e136cf271e84e18236ecc176d843
bigjang <dhex2004@gmail.com>
2023-02-28 11:38:23 +0900

    회원가입 만들기

--- src/main/java/shop/mtcoding/bankapp/model/user/UserRepository.java
+++ src/main/java/shop/mtcoding/bankapp/model/user/UserRepository.java
@@ -3,9 +3,11 @@ package shop.mtcoding.bankapp.model.user;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
+import shop.mtcoding.bankapp.dto.user.JoinReqDto;
+
 @Mapper
 public interface UserRepository {
-    public int insert(User user);
+    public int insert(JoinReqDto joinReqDto);
 
     public int updateById(User user);
 
